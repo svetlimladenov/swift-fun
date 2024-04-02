4f768b61856688377c26f0ef17221d3a38471211
svetlimladenov <svetlimladenov@gmail.com>
2024-04-02 14:14:18 +0300

    Update the crash course playgrounds

--- crash-course/Functions.playground/Contents.swift
+++ crash-course/Functions.playground/Contents.swift
@@ -6,3 +6,10 @@ func Test(external firstName: String, secondName: String, age : Int = 20) -> Str
 
 let result = Test(external: "svetlin", secondName: "Mladenov")
 
+
+func Hello(name: String) {
+    print("Hello mr.\(name)")
+}
+
+
+Hello(name: "ivan")
